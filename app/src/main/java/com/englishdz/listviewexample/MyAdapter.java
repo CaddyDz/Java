0e9fd4224003a95232c01a0868cba019ced4d1f6
Caddy Dz <tbitw31@gmail.com>
2017-10-13 10:34:04 +0100

    Sync
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ app/src/main/java/com/englishdz/listviewexample/MyAdapter.java
@@ -0,0 +1,42 @@
+package com.englishdz.listviewexample;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by GREY on 10/12/17.
+ */
+
+class MyAdapter extends ArrayAdapter<String> {
+
+    public MyAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout2);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater theInflater = LayoutInflater.from(getContext());
+
+        View theView = theInflater.inflate(R.layout.row_layout2, parent, false);
+
+        String channel = getItem(position);
+
+        TextView theTextView = (TextView) theView.findViewById(R.id.textView1);
+
+        theTextView.setText(channel);
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.grey);
+
+        theImageView.setImageResource(R.drawable.grey);
+
+        return theView;
+    }
+}
