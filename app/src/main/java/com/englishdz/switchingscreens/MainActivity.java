3b66df3bc2bf4eea22695133e61ffbb1868ef7d1
Caddy Dz <tbitw31@gmail.com>
2017-10-16 13:00:40 +0100

    Pass data between mutiple activities 1- Create intent on click event listener method to initialize a second activity 2- Define custom activity in AndroidManifest.xml 3- Return exit status integer on activity close event 4- Put extra data on the intent calling other activity 5- Start activity with/without response 6- Specify activity layout onCreate of activity using setContentView(layout) method 7- Get extra data from the intent calling other activity 8- Append string to TextView 9- Get data from second activity 10- Set result of an intent
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- app/src/main/java/com/englishdz/switchingscreens/MainActivity.java
+++ app/src/main/java/com/englishdz/switchingscreens/MainActivity.java
@@ -1,7 +1,11 @@
 package com.englishdz.switchingscreens;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onGetNameClick(View view) {
+        Intent getNameScreenIntent = new Intent(this, SecondScreen.class);
+
+        final int result = 1;
+
+        getNameScreenIntent.putExtra("callingActivity", "MainActivity");
+
+        startActivityForResult(getNameScreenIntent, result);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView usersNameMessage = (TextView) findViewById(R.id.users_name_message);
+        String nameSentBack = data.getStringExtra("UsersName");
+
+        usersNameMessage.append(" " + nameSentBack);
     }
 }
---
+++ app/src/main/java/com/englishdz/switchingscreens/SecondScreen.java
@@ -0,0 +1,42 @@
+package com.englishdz.switchingscreens;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by GREY on 10/16/17.
+ */
+
+public class SecondScreen extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+        Intent activityThatCalled = getIntent();
+
+        String previousActivity = activityThatCalled.getExtras().getString("callingActivity");
+
+        TextView callingActivityMessage = (TextView) findViewById(R.id.calling_activity_info_text_view);
+        callingActivityMessage.append(" " + previousActivity);
+    }
+
+    public void onSendUsersName(View view) {
+        EditText usersNameET = (EditText) findViewById(R.id.users_name_edit_text);
+
+        String usersName = String.valueOf(usersNameET.getText());
+
+        Intent goingBack = new Intent();
+
+        goingBack.putExtra("UsersName", usersName);
+
+        setResult(RESULT_OK, goingBack);
+        finish();
+    }
+
+}
